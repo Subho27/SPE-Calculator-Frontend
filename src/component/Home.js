d3ac859ecad8add26146fdce608ebacb31e0589a
Subhodip Rudra <42699151+Subho27@users.noreply.github.com>
2024-02-20 15:47:11 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
       
       if (operation === 'power') {
           try {
-          axios.get(`http://13.60.40.154:8085/calculator/${operation}`, {
+          axios.get(`http://16.171.132.80:8085/calculator/${operation}`, {
           params: {
             number: number,
             exponent: exponent
